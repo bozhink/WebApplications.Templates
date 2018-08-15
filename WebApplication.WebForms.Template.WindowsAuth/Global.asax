51ff82f6166a9686ce43378597a5d08c88caa86f
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-08-15 13:33:01 +0300

    Add WebApplication.WebForms.Template.WindowsAuth

---
+++ WebApplication.WebForms.Template.WindowsAuth/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication.WebForms.Template.WindowsAuth.Global" Language="C#" %>
---
+++ WebApplication.WebForms.Template.WindowsAuth/Site.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication.WebForms.Template.WindowsAuth
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
