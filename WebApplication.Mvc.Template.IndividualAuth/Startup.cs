a058b33ac601d72c155470df7aa3666b653a3b7a
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-08-15 13:26:45 +0300

    Add WebApplication.Mvc.Template.IndividualAuth

---
+++ WebApplication.Mvc.Template.IndividualAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication.Mvc.Template.IndividualAuth.Startup))]
+namespace WebApplication.Mvc.Template.IndividualAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
