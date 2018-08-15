cd946259537c06b6eb1fcd264b61a27d58acb607
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2018-08-15 13:41:55 +0300

    Add core projects

---
+++ WebApplication.MvcCore.NoAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication.MvcCore.NoAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
